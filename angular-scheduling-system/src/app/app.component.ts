019c06cb4a057aeece3ddf53520598dee1a0463c
Ayekus <aekus.trehan@gmail.com>
2022-01-17 22:04:50 -0500

    Started navigation + styling

--- angular-scheduling-system/src/app/app.component.ts
+++ angular-scheduling-system/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.router.events.subscribe((res) => {
+      console.log(this.router.url, 'Current URL');
+      if (this.router.url == '/dashboard') {
+        this.isLoggedIn = true;
+        this.isManager = true;
+      } else if (this.router.url == '/employee-dashboard') {
+        this.isLoggedIn = true;
+        this.isManager = false;
+      } else {
+        this.isLoggedIn = false;
+        this.isManager = false;
+      }
+    });
+
+    console.log(this.router.url);
+  }
   title = 'angular-scheduling-system';
+
+  userProfile: any;
+  isManager: any;
+  isLoggedIn: any;
+
+  ngOnInit(): void {}
+
+  signOut() {
+    this.router.navigate(['/sign-out']);
+  }
+
+  signIn() {
+    this.router.navigate(['/sign-in']);
+  }
+
+  dashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  profile() {
+    console.log('works');
+  }
+
+  employeeList() {
+    console.log('works');
+  }
+
+  contactManager() {
+    console.log('works');
+  }
 }
