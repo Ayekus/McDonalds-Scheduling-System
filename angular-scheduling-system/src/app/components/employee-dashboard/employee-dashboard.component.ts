e8c71f74fa94efc1dd6a7e79f786e794a0c5f8bd
Ayekus <aekus.trehan@gmail.com>
2022-01-18 00:20:26 -0500

    Fixed date picking bug + finished styling employee dashboard

--- angular-scheduling-system/src/app/components/employee-dashboard/employee-dashboard.component.ts
+++ angular-scheduling-system/src/app/components/employee-dashboard/employee-dashboard.component.ts
@@ -21,6 +21,7 @@ export class EmployeeDashboardComponent implements OnInit {
     firstDay: moment.localeData().firstDayOfWeek(),
   };
 
+  dateIsValid = true;
   userProfile: any;
   dateTouched = false;
   isWorking: any;
@@ -42,57 +43,67 @@ export class EmployeeDashboardComponent implements OnInit {
   }
 
   refreshData() {
-    console.log(this.userProfile);
-    const userUid = this.userProfile.uid;
-    const training = this.userProfile.training;
-    this.scheduledTime = undefined;
-    this.dateTouched = true;
-    this.selectedDate = moment(this.selected.startDate).format('YYYY-MM-DD');
+    if (this.selected.startDate) {
+      console.log('exsists');
+      console.log(this.selected.startDate);
+      console.log(this.userProfile);
+      const userUid = this.userProfile.uid;
+      const training = this.userProfile.training;
+      this.scheduledTime = undefined;
+      this.dateTouched = true;
+      this.selectedDate = moment(this.selected.startDate).format('YYYY-MM-DD');
 
-    this.afFirestore
-      .collection('scheduleCollection')
-      .doc(this.selectedDate)
-      .ref.get()
-      .then((doc) => {
-        console.log(doc);
-        console.log(doc.data());
-        if (doc.data()) {
-          var data: any = doc.data();
-          data = data[training];
-          console.log(data);
-          console.log(data[0]);
-          for (let i = 0; i < 5; i++) {
-            if (data[i] == userUid) {
-              console.log(data[i]);
-              console.log(i);
-              switch (i) {
-                case 0:
-                  this.scheduledTime = '5:00am - 9:00am';
-                  break;
-                case 1:
-                  this.scheduledTime = '9:00am - 1:00pm';
-                  break;
-                case 2:
-                  this.scheduledTime = '1:00pm - 5:00pm';
-                  break;
-                case 3:
-                  this.scheduledTime = '5:00pm - 9:00pm';
-                  break;
-                case 4:
-                  this.scheduledTime = '8:00pm - 11:00pm';
-                  break;
+      this.afFirestore
+        .collection('scheduleCollection')
+        .doc(this.selectedDate)
+        .ref.get()
+        .then((doc) => {
+          console.log(doc);
+          console.log(doc.data());
+          if (doc.data()) {
+            var data: any = doc.data();
+            data = data[training];
+            console.log(data);
+            console.log(data[0]);
+            for (let i = 0; i < 5; i++) {
+              if (data[i] == userUid) {
+                console.log(data[i]);
+                console.log(i);
+                switch (i) {
+                  case 0:
+                    this.scheduledTime = '5:00am - 9:00am';
+                    break;
+                  case 1:
+                    this.scheduledTime = '9:00am - 1:00pm';
+                    break;
+                  case 2:
+                    this.scheduledTime = '1:00pm - 5:00pm';
+                    break;
+                  case 3:
+                    this.scheduledTime = '5:00pm - 9:00pm';
+                    break;
+                  case 4:
+                    this.scheduledTime = '8:00pm - 11:00pm';
+                    break;
+                }
+                this.isWorking = true;
               }
-              this.isWorking = true;
             }
-          }
-          if (!this.scheduledTime) {
-            console.log('here');
+            if (!this.scheduledTime) {
+              console.log('here');
+              this.isWorking = false;
+            }
+          } else {
+            console.log('here2');
             this.isWorking = false;
           }
-        } else {
-          console.log('here2');
-          this.isWorking = false;
-        }
-      });
+        });
+    } else {
+      console.log('!exsists');
+      this.dateIsValid = false;
+      setTimeout(() => {
+        this.dateIsValid = true;
+      }, 3500);
+    }
   }
 }
